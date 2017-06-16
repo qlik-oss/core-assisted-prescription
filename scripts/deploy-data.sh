796129917d408c7b0b466f1df73ed2c657867349
Mattias Wennmo <mow@qlik.com>
2017-06-16 08:57:06 +0200

    Only deploy data to workers if it does not exist

--- scripts/deploy-data.sh
+++ scripts/deploy-data.sh
@@ -12,6 +12,11 @@ echo "========================================================================"
 
 for i in $(seq 1 $WORKERS); do
   MACHINE=$USERNAME-docker-worker$i
-  echo "-> deploying data to $MACHINE"
-  docker-machine scp -r ./data $MACHINE:/home/docker/
+
+  # Only deploy data if it does not already exists on host
+  docker-machine ssh $MACHINE "ls /home/docker/data" &> /dev/null
+  if [ $? -eq 1 ]; then
+    echo "-> deploying data to $MACHINE"
+    docker-machine scp -r ./data $MACHINE:/home/docker/
+  fi
 done
