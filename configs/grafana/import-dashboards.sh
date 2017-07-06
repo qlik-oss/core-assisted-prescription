b6c5ad56682ed9e49f39b14d9381a281a624f9de
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-07-06 17:00:52 +0200

    dashboards work

--- configs/grafana/import-dashboards.sh
+++ configs/grafana/import-dashboards.sh
@@ -7,7 +7,6 @@
 if [ ${DASHBOARDS_IMPORT} ]; then
 
     dashboard=$(echo $DASHBOARDS_IMPORT | tr "," "\n")
-    echo "$dashboard"
     for dash in $dashboard
     do
         dashboard_id=${dash/:*/}
@@ -15,12 +14,12 @@ if [ ${DASHBOARDS_IMPORT} ]; then
 
         echo "DASH ID: $dashboard_id - $dashboard_version"
         curl -s "https://grafana.net/api/dashboards/${dashboard_id}/revisions/${dashboard_version}/download" > /tmp/dashboard_data.json
-        echo "{ \"dashboard\": "$(cat /tmp/dashboard_data.json)", \"overwrite\": true }" > /tmp/dashboard_data2.json
-        sed -i.bak "s/\\${DS_PROMETHEUS}/${PROMETHEUS_ENDPOINT}/g" /tmp/dashboard_data2.json
+        echo "{ \"dashboard\": $(cat /tmp/dashboard_data.json), \"overwrite\": true }" > /tmp/dashboard_data2.json
+        sed 's/${DS_PROMETHEUS}/Prometheus/g' /tmp/dashboard_data2.json > /tmp/dashboard_data3.json
 set -ex
         curl \
             -X POST -H 'Content-Type: application/json;charset=UTF-8' \
-            -d @/tmp/dashboard_data2.json \
+            -d @/tmp/dashboard_data3.json \
             "http://${GF_SECURITY_ADMIN_USER}:${GF_SECURITY_ADMIN_PASSWORD}@localhost:3000/api/dashboards/db"
     done
 fi
