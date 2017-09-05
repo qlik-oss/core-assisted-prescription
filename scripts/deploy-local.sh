93714a1323917e7072b67bcc945172fdac79ee02
Henrik Carlioth <hca@qlik.com>
2017-09-05 07:52:43 +0200

    Simplify setting of jwt in local scenario

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -6,19 +6,13 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
-if [ -z "$JWT_SECRET" ]; then
-  # Set Env. variable used as JWT secret. 
-  JWT_SECRET_FILE="./secrets/JWT_SECRET"
-  export JWT_SECRET=$(cat "$JWT_SECRET_FILE")
-fi
-
 docker-compose up -d dummy-data
 
 docker cp ./data/csv/. dummy-data:/data
 docker cp ./data/doc/. dummy-data:/doc
 docker cp ./secrets/. dummy-data:/secrets
 
-docker-compose up -d
+JWT_SECRET=$(cat ./secrets/JWT_SECRET) docker-compose up -d
 echo
 echo
 echo "CUSTOM ANALYTICS can be reached at https://localhost/"
