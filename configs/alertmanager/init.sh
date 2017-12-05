fa2141c7321141bf47042f40b19ed58ecbbcef01
Henrik Carlioth <hca@qlik.com>
2017-12-05 14:22:32 +0100

    adding slack webhook as a secret

--- configs/alertmanager/init.sh
+++ configs/alertmanager/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-cat /tmp/config.yml | sed "s@<slackapi>@'$SLACK_API'@g" > /etc/alertmanager/config.yml
+SLACK_WEBHOOK=`cat /run/secrets/slack_webhook`
+cat /tmp/config.yml | sed "s@<slackapi>@'$SLACK_WEBHOOK'@g" > /etc/alertmanager/config.yml
 
 # Will add path to alertmanager and passed commands (config file) after.
 set -- "/bin/alertmanager" "$@"
